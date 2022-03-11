f55ab90587b79e9c0ddae7d89ac81520c5ca454f
JagadishGosala <jagadish.gosala@gmail.com>
2022-03-11 11:44:04 +0530

    added mongoose and express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('./database/mongoose');
+// app.listen(3000, function(){
+//     console.log("Server started");
+// })
+
+app.listen(3000, ()=> {
+    console.log("Something exclusive");
+})
---
+++ database/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb')
+    .then(()=>{
+        console.log("DB connected");
+    })
+    .catch((error)=>{
+        console.log("not connected");
+    });
+
+module.exports = mongoose;
